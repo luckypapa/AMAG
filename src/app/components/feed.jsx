26c2d5f18b58c3e5a4ae6ed0f8590b4b135d9686
Bongseok Choi <bschoi83@gmail.com>
2015-08-26 21:52:59 +0900

    Add feed page for comtents
    Add feed page initially using card component.

--- src/app/components/feed.jsx
+++ src/app/components/feed.jsx
@@ -2,110 +2,152 @@
 
 var React = require('react');
 var mui = require('material-ui');
-var RaisedButton = mui.RaisedButton;
-var ThemeManager = new mui.Styles.ThemeManager();
-var AppBar = mui.AppBar;
-var Colors = mui.Styles.Colors;
-var FlatButton = mui.FlatButton;
-var FontIcon = mui.FontIcon;
-var List =  mui.List;
-var ListItem =  mui.ListItem;
-var ListDivider = mui.ListDivider;
-var IconButton = mui.IconButton;
 var Avatar = mui.Avatar;
+var Card = mui.Card;
+var CardActions = mui.CardActions;
+var CardHeader = mui.CardHeader;
+var CardText = mui.CardText;
+var ClearFix = mui.ClearFix;
+var Colors = mui.Styles.Colors;
+var IconButton = mui.IconButton;
+var TextField = mui.TextField;
 
-var ActionAssignment = require('./svg-icons/action-assignment.jsx');
-var ActionGrade = require('./svg-icons/action-grade.jsx');
-var ActionInfo = require('./svg-icons/action-info.jsx');
-var CommunicationCall = require('./svg-icons/communication-call.jsx');
-var CommunicationChatBubble = require('./svg-icons/communication-chat-bubble.jsx');
-var CommunicationEmail = require('./svg-icons/communication-email.jsx');
-var ContentDrafts = require('./svg-icons/content-drafts.jsx');
-var ContentInbox = require('./svg-icons/content-inbox.jsx');
-var ContentSend = require('./svg-icons/content-send.jsx');
-var EditorInsertChart = require('./svg-icons/editor-insert-chart.jsx');
-var FileFolder = require('./svg-icons/file-folder.jsx');
-var ToggleStarBorder = require('./svg-icons/toggle-star-border.jsx');
+var QuestionMark = require('./svg-icons/question-mark.jsx');
 
 var {Spacing, Typography} = mui.Styles;
 
+var Content = React.createClass({
+  render: function () {
+    console.log("Content render");
+    var showContent = function (content) {
+      return (
+        <div>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.<br/>
+          Donec vulputate interdum sollicitudin.<br/>
+          Nunc lacinia auctor quam sed pellentesque.<br/>
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </div>
+      );
+    }();
+    return (
+      <div className="content">
+        {showContent}
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function () {
+    console.log("Comment render");
+    var showComments = function (comment) {
+      var styles = {
+        commentStyle: {
+          marginBottom: 7,
+        },
+        commentAvatarStyle: {
+          marginRight: 10,
+        },
+      };
+      return (
+        <div style={styles.commentStyle}>
+          <Avatar size={30} style={styles.commentAvatarStyle}> A </Avatar>
+          This is a sample of comment.
+        </div>
+      );
+    }();
+    return (
+      <div className="comment">
+        {showComments}
+        {showComments}
+        {showComments}
+        {showComments}
+      </div>
+    );
+  }
+});
+
+var CardList = React.createClass({
+  render: function () {
+
+    console.log("CardList render");
+    var showCards = function () {
+      var styles = {
+        textfield: {
+          width: "100%",
+        },
+        cardtext: {
+          backgroundColor: Colors.gray300,
+          paddingBottom: 1,
+          borderTop: 'solid 1px #e0e0e0',
+        },
+        cardlist: {
+          marginTop: 15,
+        }
+      };
+      console.log("showCards render");
+      return (
+        <div style={styles.cardlist}>
+          <Card>
+            <CardHeader
+              avatar={<Avatar>A</Avatar>}
+              title="author"
+              subtitle="time" />
+            <CardText>
+              <Content data="Content"/>
+            </CardText>
+            <CardActions>
+              <IconButton tooltip="Who?">
+                <QuestionMark color={Colors.grey400} />
+              </IconButton>
+            </CardActions>
+            <CardText style={styles.cardtext}>
+              <Comment data="Comments"/>
+              <TextField
+                hintText="It will be read by some friends of yours."
+                style={styles.textfield}/>
+            </CardText>
+          </Card>
+        </div>
+      );
+    }();
+    console.log("showCards end");
+    return (
+      <div className="cardList">
+        {showCards}
+        {showCards}
+        {showCards}
+      </div>
+    );
+  }
+});
+
 var Feed = React.createClass({
+  loadContent: function () {
+    // TODO: show data of content from server
+    // this.setState({data: {content:{author:"", time:"", text:""}}});
+  },
+
+  componentDidMount: function () {
+    this.loadContent();
+  },
+
+  getInitialState: function () {
+    return {data:[]};
+  },
 
   render: function() {
-
     var containerStyle = {
-      paddingTop: Spacing.desktopKeylineIncrement
+      paddingTop: Spacing.desktopKeylineIncrement + 10,
+      paddingBottom: 15,
+      maxWidth: '650px',
+      margin: '0 auto',
     };
 
     return (
-     <div style={containerStyle}>
-      <List>
-        <ListItem
-          leftAvatar={<Avatar src="./image/ok-128.jpg" />}
-          rightIconButton={<IconButton><ToggleStarBorder color={Colors.grey400} /></IconButton>}
-          secondaryText={
-            <p>
-              <span style={{color: Colors.darkBlack}}>Brunch this weekend?</span><br/>
-              I&apos;ll be in your neighborhood doing errands this weekend.
-              Do you want to grab brunch?
-            </p>
-          }
-          secondaryTextLines={2}>
-          Brendan Lim
-        </ListItem>
-        <ListDivider inset={true} />
-        <ListItem
-          leftAvatar={<Avatar src="./image/ok-128.jpg" />}
-          rightIconButton={<IconButton><ToggleStarBorder color={Colors.grey400} /></IconButton>}
-          secondaryText={
-            <p>
-              <span style={{color: Colors.darkBlack}}>Summer BBQ</span><br/>
-              Wish I could come, but I&apos;m out of town this weekend.
-            </p>
-          }
-          secondaryTextLines={2}>
-          me, Scott, Jennifer
-        </ListItem>
-        <ListDivider inset={true} />
-        <ListItem
-          leftAvatar={<Avatar src="./image/ok-128.jpg" />}
-          rightIconButton={<IconButton><ToggleStarBorder color={Colors.grey400} /></IconButton>}
-          secondaryText={
-            <p>
-              <span style={{color: Colors.darkBlack}}>Oui oui</span><br/>
-              Do you have any Paris recs? Have you ever been?
-            </p>
-          }
-          secondaryTextLines={2}>
-          Grace Ng
-        </ListItem>
-        <ListDivider inset={true} />
-        <ListItem
-          leftAvatar={<Avatar src="./image/ok-128.jpg" />}
-          rightIconButton={<IconButton><ToggleStarBorder color={Colors.grey400} /></IconButton>}
-          secondaryText={
-            <p>
-              <span style={{color: Colors.darkBlack}}>Birthday gift</span><br/>
-              Do you have any ideas what we can get Heidi for her birthday? How about a pony?
-            </p>
-          }
-          secondaryTextLines={2}>
-          Kerem Suer
-        </ListItem>
-        <ListDivider inset={true} />
-        <ListItem
-          leftAvatar={<Avatar src="./image/ok-128.jpg" />}
-          rightIconButton={<IconButton><ToggleStarBorder color={Colors.grey400} /></IconButton>}
-          secondaryText={
-            <p>
-              <span style={{color: Colors.darkBlack}}>Recipe to try</span><br/>
-              We should eat this: grated squash. Corn and tomatillo tacos.
-            </p>
-          }
-          secondaryTextLines={2}>
-          Raquel Parrado
-        </ListItem>
-      </List>
+      <div style={containerStyle}>
+        <CardList/>
       </div>
     );
   },
